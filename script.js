70086fec323e6cc60c4f3f3d9708221b6ec88ea8
saidov7 <jaxasaidov29@gmail.com>
2025-03-18 22:14:12 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+// 1
+// let a = parseInt(prompt("Birinchi sonni kiriting:"));
+// let b = parseInt(prompt("Ikkinchi sonni kiriting:"));
+// let c = parseInt(prompt("Uchinchi sonni kiriting:"));
+// let count = 0;
+// if (a > 0) count++;
+// if (b > 0) count++;
+// if (c > 0) count++;
+// console.log("Musbat sonlar soni:", count);
+
+// 2
+// let a = parseInt(prompt("Birinchi sonni kiriting:"));
+// let b = parseInt(prompt("Ikkinchi sonni kiriting:"));
+// if (a < b) {
+//     console.log("Kichik sonning tartib raqami: 1");
+// } else {
+//     console.log("Kichik sonning tartib raqami: 2");
+// }
+
+
+// 3
+// let a = parseInt(prompt("Birinchi sonni kiriting:"));
+// let b = parseInt(prompt("Ikkinchi sonni kiriting:"));
+
+// if (a < b) {
+//     console.log("Kichik sonning tartib raqami: 1");
+// } else {
+//     console.log("Kichik sonning tartib raqami: 2");
+// }
+
+
+// 4
+// let A = parseInt(prompt("A sonini kiriting:"));
+// let B = parseInt(prompt("B sonini kiriting:"));
+// if (A !== B) {
+//     A = B = Math.max(A, B);
+// } else {
+//     A = B = 0;
+// }
+// console.log("A:", A);
+// console.log("B:", B);
+
+
+// 5
+// let a = parseInt(prompt("Birinchi sonni kiriting:"));
+// let b = parseInt(prompt("Ikkinchi sonni kiriting:"));
+// let c = parseInt(prompt("Uchinchi sonni kiriting:"));
+// let middle;
+// if ((a > b && a < c) || (a > c && a < b)) {
+//     middle = a;
+// } else if ((b > a && b < c) || (b > c && b < a)) {
+//     middle = b;
+// } else {
+//     middle = c;
+// }
+// console.log("O'rtacha son:", middle);
+
+
+// 7
+// let year = parseInt(prompt("Yilni kiriting:"));
+// if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+//     console.log(year + " - Kabisa yili, 366 kun bor.");
+// } else {
+//     console.log(year + " - Kabisa yil emas, 365 kun bor.");
+// }
+
